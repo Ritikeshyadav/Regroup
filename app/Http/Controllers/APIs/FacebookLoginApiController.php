80d48fafcac07beb596230ff8a4008fae3e5d194
Hritikkk9 <hritikdhanawade3@gmail.com>
2024-07-15 12:58:11 +0530

    import in facebook

--- app/Http/Controllers/APIs/FacebookLoginApiController.php
+++ app/Http/Controllers/APIs/FacebookLoginApiController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers\APIs;
 
 use App\Http\Controllers\Controller;
+use App\Models\IamPrincipal;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 
 class FacebookLoginApiController extends Controller
